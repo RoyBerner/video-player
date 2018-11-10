ad936f413bbd9755233f03903ee9c8613007d386
Roy Berner <r.p.berner@me.com>
2018-11-10 22:59:14 +0100

    Initial Commit

---
+++ video player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  video player
+//
+//  Created by Roy Berner on 10/11/2018.
+//  Copyright © 2018 Roy Berner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
